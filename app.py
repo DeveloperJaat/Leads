edb5843f8845420b653e89b764bc542112ccbb40
DeveloperJaat <97823189+DeveloperJaat@users.noreply.github.com>
2025-02-26 20:10:34 +0530

    17

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import requests
 app = Flask(__name__)
 
 # ✅ Google Search API Key & Search Engine ID
-GOOGLE_SEARCH_API_KEY = ""  # Replace with your actual API key
-SEARCH_ENGINE_ID = ""  # Replace with your actual Search Engine ID
+GOOGLE_SEARCH_API_KEY = "AIzaSyBMFL8kL6li79HITDgVhww3sp6V8ko52No"  # Replace with your actual API key
+SEARCH_ENGINE_ID = "7603a783722314f9e"  # Replace with your actual Search Engine ID
 
 @app.route("/", methods=["GET", "POST"])
 def home():
